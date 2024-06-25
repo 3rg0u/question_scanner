e268c3a758730eaba8e1f71267c18a018f5406c7
gy___ <nqaisme@icloud.com>
2024-06-25 07:55:03 +0700

    fix scan inccorect questions

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const fs = require('fs')
-const { JSDOM } = require('jsdom');
-const path = require('path');
+const { JSDOM } = require('jsdom')
+const path = require('path')
 
-
+let directory = path.join(__dirname, 'lecture1')
+let output = path.join(__dirname, 'test.txt')
 class Question {
     quest; ansA; ansB; ansC; ansD; correctAns;
     constructor(quest, ansA, ansB, ansC, ansD, correctAns) {
@@ -47,10 +48,11 @@ function createQuestion(question, ans, corrAns) {
     ans.forEach(answer => {
         innerAns.push(answer.innerHTML.replace(/[\n\r\t]/g, '').trim())
     })
+    // console.log(ans);
     a1 = `"'${ans[0].innerHTML.replace(/[\n\r\t]/g, '').trim()}"`
     a2 = `"'${ans[1].innerHTML.replace(/[\n\r\t]/g, '').trim()}"`
     a3 = `"'${ans[2].innerHTML.replace(/[\n\r\t]/g, '').trim()}"`
-    a4 = `"'${ans[3].innerHTML.replace(/[\n\r\t]/g, '').trim()}"`
+    // a4 = `"'${ans[3].innerHTML.replace(/[\n\r\t]/g, '').trim()}"`
 
     cA = (innerAns.indexOf(corrAns.innerHTML.replace(/[\n\r\t]/g, '').trim()) + 1)
 
@@ -58,28 +60,29 @@ function createQuestion(question, ans, corrAns) {
 }
 
 try {
-    const htmlList = fs.readdirSync('./htmls/').filter(file => path.extname(file).toLowerCase() === '.html')
+    const htmlList = fs.readdirSync(directory).filter(file => path.extname(file).toLowerCase() === '.html')
     htmlList.forEach(htmlFile => {
         try {
-            const htmldoc = fs.readFileSync('./htmls/' + htmlFile, 'utf-8')
+
+            const htmldoc = fs.readFileSync(path.join(directory, htmlFile), 'utf-8')
             const document = new JSDOM(htmldoc).window.document
-            document.querySelectorAll('.text').forEach(questionblock => {
-
-
-                const question = questionblock.querySelector('.question_text')
-                const ans = questionblock.querySelectorAll('.answer_text')
-                const corrAns = questionblock.querySelector('.selected_answer .answer_text')
-                let newQuestion = createQuestion(question, ans, corrAns)
-                if (!questionList.some(question => question.quest === newQuestion.quest)) questionList.push(newQuestion)
-
+            document.querySelectorAll('.multiple_choice_question').forEach(questionblock => {
+                if (!questionblock.classList.contains('incorrect')) {
+                    const question = questionblock.querySelector('.question_text')
+                    const ans = questionblock.querySelectorAll('.answer_text')
+                    const corrAns = questionblock.querySelector('.selected_answer .answer_text')
+                    console.log(ans.length);
+                    let newQuestion = createQuestion(question, ans, corrAns)
+                    if (!questionList.some(question => question.quest === newQuestion.quest)) questionList.push(newQuestion)
+                }
             })
         } catch (error) {
             console.log(error);
         }
     })
-    console.log(questionList.length);
     questionList.forEach(question => {
-        fs.appendFileSync('./nqa.csv', question.toFullQuestion())
+        // console.log(question.toFullQuestion());
+        // fs.appendFileSync(output, question.toFullQuestion())
     })
 } catch (error) {
 
